d9532a0a2c99b56269f31e7f54e7333ea131be34
zachplatypus <Vythrin@gmail.com>
2023-10-10 13:20:34 -0700

    adding 404 route

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+
 app.get('/', (req, res) => {
     res.send('This is text')
 })
-app.listen(3000)
+
+app.get('*', (req, res) => {
+    res.status(404).send(`<img src="https://httpstatusdogs.com/img/404.jpg" alt="404 Not Found">`)
+})
+
+app.listen(process.env.PORT)
