6e8b2a5b1398f400ffff5b214d4be77c49c7b59e
zachplatypus <Vythrin@gmail.com>
2023-10-23 21:17:48 -0700

    part 7 complete

--- controllers/places.js
+++ controllers/places.js
@@ -26,7 +26,24 @@ router.get('/:id', (req, res) => {
 })
 
 router.put('/:id', (req, res) => {
-    res.send('PUT /places/:id stub')
+    let id = Number(req.params.id)
+    if (isNaN(id)){
+        res.render('error404')
+    } else if (!places[id]){
+        res.render('error404')
+    } else {
+        if (!req.body.pic){
+            req.body.pic = 'https://placekitten.com/422/422'
+        }
+        if (!req.body.city){
+            req.body.city = 'Happytown'
+        }
+        if (!req.body.state){
+            req.body.state = 'BD'
+        }
+        places[id] = req.body
+        res.redirect(`/places/${id}`)
+    }
 })
 
 router.delete('/:id', (req, res) => {
@@ -55,14 +72,14 @@ router.get('/:id/edit', (req, res) =>{
 })
 
 router.post('/', (req, res) => {
+    if (!req.body.pic){
+        req.body.pic = 'https://placekitten.com/422/422'
+    }
     if (!req.body.city){
         req.body.city = 'Happytown'
     }
     if (!req.body.state){
         req.body.state = 'BD'
-    }
-    if (!req.body.pic){
-        req.body.pic = 'https://placekitten.com/422/422'
     }
     places.push(req.body)
     res.redirect('/places')
